e6e69f3b6e035672c82ea450dbcbe44799ace1a0
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-11-10 20:13:53 +0300

    Update

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api", // Flask backend
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000/api",
 });
